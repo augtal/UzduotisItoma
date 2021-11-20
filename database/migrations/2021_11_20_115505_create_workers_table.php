e3a05b9834685af1021def7e15c1418f6cc2835a
augtal <augustas.talacka@gmail.com>
2021-11-20 14:17:16 +0200

    Added database to project

--- database/migrations/2021_11_20_115505_create_workers_table.php
+++ database/migrations/2021_11_20_115505_create_workers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateWorkersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('workers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->integer('role'); // 65 is admin, 1 is user
-            $table->rememberToken();
+            $table->string('email');
+            $table->string('phone');
+            $table->foreignId('company');
             $table->timestamps();
+
+            $table->foreign('company')->references('id')->on('companies');
         });
     }
 
@@ -32,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('workers');
     }
 }
