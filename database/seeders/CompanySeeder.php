86babadd1b3cb35c18232150949dca390fcc6c58
augtal <augustas.talacka@gmail.com>
2021-11-20 17:41:52 +0200

    Added database seeding

--- database/seeders/CompanySeeder.php
+++ database/seeders/CompanySeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class CompanySeeder extends Seeder
 {
@@ -13,6 +14,25 @@ class CompanySeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('companies')->insert([
+            'name' => "Imone1",
+            'email' => "imone1@imone.lt",
+            'URL' => 'imone1.lt',
+            'image' => 'imone1',
+        ]);
+
+        DB::table('companies')->insert([
+            'name' => "Imone2",
+            'email' => "imone2@imone.lt",
+            'URL' => 'imone2.lt',
+            'image' => 'imone2',
+        ]);
+
+        DB::table('companies')->insert([
+            'name' => "Imone3",
+            'email' => "imone3@imone.lt",
+            'URL' => 'imone3.lt',
+            'image' => 'imone3',
+        ]);
     }
 }
--- database/seeders/WorkerSeeder.php
+++ database/seeders/WorkerSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class WorkerSeeder extends Seeder
 {
@@ -13,6 +14,41 @@ class WorkerSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('workers')->insert([
+            'name' => 'Darbuotojas11',
+            'email' => 'darbuotojas11@gmail.com',
+            'phone' => '+37061111111',
+            'company' => 1,
+        ]);
+        DB::table('workers')->insert([
+            'name' => 'Darbuotojas12',
+            'email' => 'darbuotojas12@gmail.com',
+            'phone' => '+37061111112',
+            'company' => 1,
+        ]);
+        DB::table('workers')->insert([
+            'name' => 'Darbuotojas21',
+            'email' => 'darbuotojas21@gmail.com',
+            'phone' => '+37062222221',
+            'company' => 2,
+        ]);
+        DB::table('workers')->insert([
+            'name' => 'Darbuotojas22',
+            'email' => 'darbuotojas22@gmail.com',
+            'phone' => '+37062222222',
+            'company' => 2,
+        ]);
+        DB::table('workers')->insert([
+            'name' => 'Darbuotojas23',
+            'email' => 'darbuotojas23@gmail.com',
+            'phone' => '+37062222223',
+            'company' => 2,
+        ]);
+        DB::table('workers')->insert([
+            'name' => 'Darbuotojas31',
+            'email' => 'darbuotojas31@gmail.com',
+            'phone' => '+37063333331',
+            'company' => 3,
+        ]);
     }
 }
