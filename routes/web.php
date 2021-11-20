ba29d5d9e98a526bf6dded7ce4575654765121a4
augtal <augustas.talacka@gmail.com>
2021-11-20 13:49:10 +0200

    Removed register functionality

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes([
+    'register' => false,
+]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
