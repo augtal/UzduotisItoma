80bddc4488635c73e8361284c233eaa412bceacf
augtal <augustas.talacka@gmail.com>
2021-11-21 18:12:48 +0200

    Imporved database seeding

--- database/seeders/CompanySeeder.php
+++ database/seeders/CompanySeeder.php
@@ -17,22 +17,92 @@ class CompanySeeder extends Seeder
         DB::table('companies')->insert([
             'name' => "Imone1",
             'email' => "imone1@imone.lt",
-            'url' => 'imone1.lt',
-            'logo' => 'imone1',
+            'url' => 'https://www.imone1.lt',
+            'logo' => 'imone1.png',
         ]);
-
         DB::table('companies')->insert([
             'name' => "Imone2",
             'email' => "imone2@imone.lt",
-            'url' => 'imone2.lt',
-            'logo' => 'imone2',
+            'url' => 'https://www.imone2.lt',
+            'logo' => 'imone2.png',
         ]);
-
         DB::table('companies')->insert([
             'name' => "Imone3",
             'email' => "imone3@imone.lt",
-            'url' => 'imone3.lt',
-            'logo' => 'imone3',
+            'url' => 'https://www.imone3.lt',
+            'logo' => 'imone3.png',
+        ]);
+        DB::table('companies')->insert([
+            'name' => "Imone4",
+            'email' => "imone4@imone.lt",
+            'url' => 'https://www.imone4.lt',
+            'logo' => 'imone4.png',
+        ]);
+        DB::table('companies')->insert([
+            'name' => "Imone5",
+            'email' => "imone5@imone.lt",
+            'url' => 'https://www.imone5.lt',
+            'logo' => 'imone5.png',
+        ]);
+        DB::table('companies')->insert([
+            'name' => "Imone6",
+            'email' => "imone6@imone.lt",
+            'url' => 'https://www.imone6.lt',
+            'logo' => 'imone6.png',
+        ]);
+        DB::table('companies')->insert([
+            'name' => "Imone7",
+            'email' => "imone7@imone.lt",
+            'url' => 'https://www.imone7.lt',
+            'logo' => 'imone7.png',
+        ]);
+        DB::table('companies')->insert([
+            'name' => "Imone8",
+            'email' => "imone8@imone.lt",
+            'url' => 'https://www.imone8.lt',
+            'logo' => 'imone8.png',
+        ]);
+        DB::table('companies')->insert([
+            'name' => "Imone9",
+            'email' => "imone9@imone.lt",
+            'url' => 'https://www.imone9.lt',
+            'logo' => 'imone9.png',
+        ]);
+        DB::table('companies')->insert([
+            'name' => "Imone10",
+            'email' => "imone10@imone.lt",
+            'url' => 'https://www.imone10.lt',
+            'logo' => 'imone10.png',
+        ]);
+        DB::table('companies')->insert([
+            'name' => "Imone11",
+            'email' => "imone11@imone.lt",
+            'url' => 'https://www.imone11.lt',
+            'logo' => 'imone11.png',
+        ]);
+        DB::table('companies')->insert([
+            'name' => "Imone12",
+            'email' => "imone12@imone.lt",
+            'url' => 'https://www.imone12.lt',
+            'logo' => 'imone12.png',
+        ]);
+        DB::table('companies')->insert([
+            'name' => "Imone13",
+            'email' => "imone13@imone.lt",
+            'url' => 'https://www.imone13.lt',
+            'logo' => 'imone13.png',
+        ]);
+        DB::table('companies')->insert([
+            'name' => "Imone14",
+            'email' => "imone14@imone.lt",
+            'url' => 'https://www.imone14.lt',
+            'logo' => 'imone14.png',
+        ]);
+        DB::table('companies')->insert([
+            'name' => "Imone15",
+            'email' => "imone15@imone.lt",
+            'url' => 'https://www.imone15.lt',
+            'logo' => 'imone15.png',
         ]);
     }
 }
--- database/seeders/WorkerSeeder.php
+++ database/seeders/WorkerSeeder.php
@@ -50,5 +50,169 @@ class WorkerSeeder extends Seeder
             'phone' => '+37063333331',
             'company' => 3,
         ]);
+        DB::table('workers')->insert([
+            'name' => 'Darbuotojas32',
+            'email' => 'darbuotojas32@gmail.com',
+            'phone' => '+37061111132',
+            'company' => 3,
+        ]);
+        DB::table('workers')->insert([
+            'name' => 'Darbuotojas33',
+            'email' => 'darbuotojas33@gmail.com',
+            'phone' => '+37061111133',
+            'company' => 3,
+        ]);
+        DB::table('workers')->insert([
+            'name' => 'Darbuotojas34',
+            'email' => 'darbuotojas34@gmail.com',
+            'phone' => '+37062222234',
+            'company' => 3,
+        ]);
+        DB::table('workers')->insert([
+            'name' => 'Darbuotojas35',
+            'email' => 'darbuotojas35@gmail.com',
+            'phone' => '+37062222235',
+            'company' => 3,
+        ]);
+        DB::table('workers')->insert([
+            'name' => 'Darbuotojas41',
+            'email' => 'darbuotojas41@gmail.com',
+            'phone' => '+37062222241',
+            'company' => 4,
+        ]);
+        DB::table('workers')->insert([
+            'name' => 'Darbuotojas42',
+            'email' => 'darbuotojas42@gmail.com',
+            'phone' => '+37063333342',
+            'company' => 4,
+        ]);
+
+
+        DB::table('workers')->insert([
+            'name' => 'Darbuotojas43',
+            'email' => 'darbuotojas43@gmail.com',
+            'phone' => '+37061111143',
+            'company' => 4,
+        ]);
+        DB::table('workers')->insert([
+            'name' => 'Darbuotojas44',
+            'email' => 'darbuotojas44@gmail.com',
+            'phone' => '+37061111144',
+            'company' => 4,
+        ]);
+        DB::table('workers')->insert([
+            'name' => 'Darbuotojas51',
+            'email' => 'darbuotojas51@gmail.com',
+            'phone' => '+37062222251',
+            'company' => 5,
+        ]);
+        DB::table('workers')->insert([
+            'name' => 'Darbuotojas52',
+            'email' => 'darbuotojas52@gmail.com',
+            'phone' => '+37062222252',
+            'company' => 5,
+        ]);
+        DB::table('workers')->insert([
+            'name' => 'Darbuotojas61',
+            'email' => 'darbuotojas61@gmail.com',
+            'phone' => '+37062222261',
+            'company' => 6,
+        ]);
+        DB::table('workers')->insert([
+            'name' => 'Darbuotojas62',
+            'email' => 'darbuotojas62@gmail.com',
+            'phone' => '+37063333362',
+            'company' => 6,
+        ]);
+        DB::table('workers')->insert([
+            'name' => 'Darbuotojas71',
+            'email' => 'darbuotojas71@gmail.com',
+            'phone' => '+37061111171',
+            'company' => 7,
+        ]);
+        DB::table('workers')->insert([
+            'name' => 'Darbuotojas81',
+            'email' => 'darbuotojas81@gmail.com',
+            'phone' => '+37061111181',
+            'company' => 8,
+        ]);
+        DB::table('workers')->insert([
+            'name' => 'Darbuotojas82',
+            'email' => 'darbuotojas82@gmail.com',
+            'phone' => '+37062222282',
+            'company' => 8,
+        ]);
+        DB::table('workers')->insert([
+            'name' => 'Darbuotojas83',
+            'email' => 'darbuotojas83@gmail.com',
+            'phone' => '+37062222283',
+            'company' => 8,
+        ]);
+        DB::table('workers')->insert([
+            'name' => 'Darbuotojas91',
+            'email' => 'darbuotojas91@gmail.com',
+            'phone' => '+37062222291',
+            'company' => 9,
+        ]);
+        DB::table('workers')->insert([
+            'name' => 'Darbuotojas92',
+            'email' => 'darbuotojas92@gmail.com',
+            'phone' => '+37063333392',
+            'company' => 9,
+        ]);
+        DB::table('workers')->insert([
+            'name' => 'Darbuotojas93',
+            'email' => 'darbuotojas93@gmail.com',
+            'phone' => '+37061111193',
+            'company' => 9,
+        ]);
+        DB::table('workers')->insert([
+            'name' => 'Darbuotojas101',
+            'email' => 'darbuotojas101@gmail.com',
+            'phone' => '+37062222101',
+            'company' => 10,
+        ]);
+        DB::table('workers')->insert([
+            'name' => 'Darbuotojas111',
+            'email' => 'darbuotojas111@gmail.com',
+            'phone' => '+37062222111',
+            'company' => 11,
+        ]);
+        DB::table('workers')->insert([
+            'name' => 'Darbuotojas112',
+            'email' => 'darbuotojas112@gmail.com',
+            'phone' => '+37062222112',
+            'company' => 11,
+        ]);
+        DB::table('workers')->insert([
+            'name' => 'Darbuotojas113',
+            'email' => 'darbuotojas113@gmail.com',
+            'phone' => '+37062222113',
+            'company' => 11,
+        ]);
+        DB::table('workers')->insert([
+            'name' => 'Darbuotojas121',
+            'email' => 'darbuotojas121@gmail.com',
+            'phone' => '+37062222121',
+            'company' => 12,
+        ]);
+        DB::table('workers')->insert([
+            'name' => 'Darbuotojas122',
+            'email' => 'darbuotojas122@gmail.com',
+            'phone' => '+37062222122',
+            'company' => 12,
+        ]);
+        DB::table('workers')->insert([
+            'name' => 'Darbuotojas131',
+            'email' => 'darbuotojas131@gmail.com',
+            'phone' => '+37062222131',
+            'company' => 13,
+        ]);
+        DB::table('workers')->insert([
+            'name' => 'Darbuotojas132',
+            'email' => 'darbuotojas132@gmail.com',
+            'phone' => '+37062222132',
+            'company' => 13,
+        ]);
     }
 }
